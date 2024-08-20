4a20a12bb07d55c553bcda33e8aead6dd71c3255
Ahmed Ali <70485778+AhmedAli777@users.noreply.github.com>
2024-08-20 09:51:42 +0300

    Add files via upload

---
+++ HAL/LCD/LCD_config.h
@@ -0,0 +1,18 @@
+﻿/*
+ * LCD_config.h
+ *
+ * Created: 14/08/2024 10:37:53 ص
+ *  Author: Ahmed Ali
+ */ 
+
+
+#ifndef LCD_CONFIG_H_
+#define LCD_CONFIG_H_
+
+#define LCD_RS   DIO_PORTD,DIO_PIN2
+#define LCD_RW   DIO_PORTD,DIO_PIN1
+#define LCD_E    DIO_PORTD,DIO_PIN0
+
+#define LCD_PORT DIO_PORTC
+
+#endif /* LCD_CONFIG_H_ */
